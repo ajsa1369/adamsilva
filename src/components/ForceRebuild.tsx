9f4520d4e923df17c3153349ce8bc993762478ca
ajsa1369 <ajstalker@gmail.com>
2025-10-20 21:41:54 -0400

    🔥 NUCLEAR: Add force rebuild component to bust all caches

---
+++ src/components/ForceRebuild.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// FORCE REBUILD: Added timestamp 1761009567390
+// This comment forces Vite to recognize file changes
+
+export const ForceRebuild: React.FC = () => {
+  return null;
+};
